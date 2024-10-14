e9b4e02607b7a924e6f6c168c170257c80ad2fc1
ollioll <cmarcelino@berkeley.edu>
2024-10-14 14:49:45 -0700

    Initial Commit

---
+++ Agent Apprentice/Agent_ApprenticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Agent_ApprenticeApp.swift
+//  Agent Apprentice
+//
+//  Created by cmello9 on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Agent_ApprenticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Agent ApprenticeTests/Agent_ApprenticeTests.swift
@@ -0,0 +1,16 @@
+//
+//  Agent_ApprenticeTests.swift
+//  Agent ApprenticeTests
+//
+//  Created by cmello9 on 10/14/24.
+//
+
+import Testing
+
+struct Agent_ApprenticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
